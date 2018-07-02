92a19d04f734630535ac002ab806dddf54292f5c
parth-gulati <gulati.parth98@gmail.com>
2018-07-02 19:50:15 +0530

    Extra print commands removed
    The extra print commands that were included to test errors and check whether the expected output is coming or not was removed.

--- src/Time_zone_change.java
+++ src/Time_zone_change.java
@@ -28,11 +28,11 @@ public class Time_zone_change {
 			  datefinal =sdf.parse(date1);      //Parsing the date from the string input passed
 			  Calendar c = Calendar.getInstance();
 			  c.setTime(datefinal);
-			  System.out.print(c.getTime());
+			
 			  TimeZone t = TimeZone.getTimeZone(location);
 			  c.setTimeZone(t);
 			  c.add(Calendar.MILLISECOND, t.getDSTSavings());
-			  System.out.print(c.getTime());
+			  
 			  SimpleDateFormat sd1 = new SimpleDateFormat("yyyyMMdd");
 		      sd1.setTimeZone(TimeZone.getTimeZone(location));
 		      SimpleDateFormat sd2 = new SimpleDateFormat("hhmmss");
@@ -41,15 +41,11 @@ public class Time_zone_change {
 		      String start_date = sd1.format(c.getTime());
 		      String start_time = sd2.format(c.getTime());
 		      
-		      System.out.println(start_time);
-		      
 		      c.add(Calendar.HOUR_OF_DAY,3); //3 hours meeting
 		      
 		      String end_date = sd1.format(c.getTime());
 		      String end_time = sd2.format(c.getTime());
-		      
-		      System.out.println("\n" + end_date);
-		      System.out.println(end_time);
+			
 		      SendMailSSL.send1(start_date,start_time,end_date,end_time,location);
 		      	
 			
